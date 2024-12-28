c4d888068fe99926033fe755b768f4003c8c217b
DaryRonsard <dhien@upb.ci>
2024-12-28 02:36:47 +0000

    use cloudinary for manage picture

--- src/rooms/models/room_models.py
+++ src/rooms/models/room_models.py
@@ -1,3 +1,4 @@
+from cloudinary.models import CloudinaryField
 from django.db import models
 from accounts.models.direction_models import DirectionModels
 from base.models.helpers.named_date_time_model import NamedDateTimeModel
@@ -7,7 +8,9 @@ class RoomsModels(NamedDateTimeModel):
     direction = models.ForeignKey(DirectionModels, on_delete=models.CASCADE)
     capacite = models.PositiveIntegerField()
     localisation = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='salles/', blank=True, null=True)
+    #image = models.ImageField(upload_to='salles/', blank=True, null=True)
+    image = CloudinaryField('salles', blank=True, null=True)
+
 
     def __str__(self):
         return f"{self.name} - {self.direction.name}"
--- src/rooms/viewset/rooms_viewset.py
+++ src/rooms/viewset/rooms_viewset.py
@@ -1,3 +1,4 @@
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet
 
@@ -5,6 +6,10 @@ from rooms.serializer.rooms_serializer import RoomsSerializer
 from rooms.models.room_models import RoomsModels
 
 
-class RoomsViewSet(ModelViewSet):
+
+class RoomsViewSet(CreateModelMixin, ListModelMixin):
     serializer_class = RoomsSerializer
     queryset = RoomsModels.objects.all()
+
+    def get_permissions(self):
+        return  [IsAuthenticated(), '''AllowAny(),''' ]
