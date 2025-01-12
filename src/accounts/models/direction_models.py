b7af94bca36e7a424e6ffca6b6194e45d5f052a2
DaryRonsard <dhien@upb.ci>
2025-01-12 18:13:17 +0000

    update image in models directionse

--- src/accounts/models/direction_models.py
+++ src/accounts/models/direction_models.py
@@ -5,7 +5,7 @@ from base.models.helpers.named_date_time_model import NamedDateTimeModel
 
 class DirectionModels(NamedDateTimeModel):
     description = models.CharField(max_length=180)
-    avatar = CloudinaryField('avatar', blank=True, null=True)
+    images = CloudinaryField('images', blank=True, null=True)
 
     def __str__(self):
         return self.name
--- src/accounts/serializer/direction_serializer.py
+++ src/accounts/serializer/direction_serializer.py
@@ -5,7 +5,7 @@ from accounts.models.direction_models import DirectionModels
 class DirectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = DirectionModels
-        fields = ('description', 'name')
+        fields = ('description', 'name', 'images')
 
     def create(self, validated_data):
         return DirectionModels.objects.create(**validated_data)
