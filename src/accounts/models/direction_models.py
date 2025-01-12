8dec429c949ec2d92be3db5c96c7dd1b4760d3c3
DaryRonsard <dhien@upb.ci>
2025-01-12 18:09:35 +0000

    add image in models directionse

--- src/accounts/models/direction_models.py
+++ src/accounts/models/direction_models.py
@@ -1,9 +1,11 @@
+from cloudinary.models import CloudinaryField
 from django.db import models
 from base.models.helpers.named_date_time_model import NamedDateTimeModel
 
 
 class DirectionModels(NamedDateTimeModel):
     description = models.CharField(max_length=180)
+    avatar = CloudinaryField('avatar', blank=True, null=True)
 
     def __str__(self):
         return self.name
